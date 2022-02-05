6f262669b3b70ac4163217d16e82ca6983b96911
PromptWash <99064557+PromptWash@users.noreply.github.com>
2022-02-05 22:21:50 +0700

    Added animation

--- js/main.js
+++ js/main.js
@@ -10,4 +10,40 @@ $(document).ready(function () {
             $("#nav-container").removeClass("active");
         }
     });
+
+    $(window).scroll(function () {
+        if (($(this).scrollTop() + $(this).height()) > $("#promotion").offset().top) {
+            $("#promotion").children().removeClass("hide");
+            $("#promotion").children().addClass("animate__animated animate__fadeIn animate__slow");
+        }
+
+        if (($(this).scrollTop() + $(this).height() - 150) > $("#punch-line").offset().top) {
+            $("#punch-line").children().removeClass("hide");
+            $("#punch-line").children().addClass("animate__animated animate__fadeIn animate__slow");
+        }
+
+        if (($(this).scrollTop() + $(this).height() - 200) > $("#pricing").offset().top) {
+            $("#pricing").children().removeClass("hide");
+            $("#pricing").children().addClass("animate__animated animate__fadeIn animate__slow");
+        }
+
+        if (($(this).scrollTop() + $(this).height() - 200) > $("#map").offset().top) {
+            $("#map").children().removeClass("hide");
+            $("#map").children("h1").addClass("animate__animated animate__fadeIn animate__slow");
+            $("#map").find("img").addClass("animate__animated animate__fadeInRight animate__slow");
+            $("#map").find("iframe").addClass("animate__animated animate__fadeInLeft animate__slow");
+        }
+
+        if (($(this).scrollTop() + $(this).height() - 200) > $("#washer").offset().top) {
+            $("#washer").children().removeClass("hide");
+            $("#washer").children("img").addClass("animate__animated animate__fadeInLeft animate__slow");
+            $("#washer").children(".content-container").addClass("animate__animated animate__fadeInRight animate__slow");
+        }
+
+        if (($(this).scrollTop() + $(this).height() - 200) > $("#dryer").offset().top) {
+            $("#dryer").children().removeClass("hide");
+            $("#dryer").children("img").addClass("animate__animated animate__fadeInRight animate__slow");
+            $("#dryer").children(".content-container").addClass("animate__animated animate__fadeInLeft animate__slow");
+        }
+    });
 });
